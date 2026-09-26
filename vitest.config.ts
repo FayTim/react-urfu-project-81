eb1c259e2f65755ce35aa3422219ee9472060fcc
FayTim <aaa.942006@gmail.com>
2026-09-26 17:27:52 +0500

    six version

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@hexlet/code': path.resolve(__dirname, 'src/index.ts'),
+        },
+    },
     test: {
         coverage: {
             provider: 'v8',
