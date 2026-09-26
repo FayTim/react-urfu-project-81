b144bf7ca19815af9ad2bb5b8b2b93534c22c58d
FayTim <aaa.942006@gmail.com>
2026-09-26 17:20:50 +0500

    five version

--- __tests__/HexcletCode.test.ts
+++ __tests__/HexcletCode.test.ts
@@ -1,17 +1,10 @@
 import { describe, expect, test } from 'vitest'
 import HexletCode from '@hexlet/code';
 import { readFixture } from '../__tests__/helpers/readFixture';
-import Tag from "../src/Tag";
+import { FormBuilder } from "../src";
 
 
 const template = { name: 'rob', job: 'hexlet', gender: 'm' };
-
-test('check hexletcode from', () => {
-    expect(new Tag("br").toString()).toEqual("<br>");
-    expect(new Tag("hr").toString()).toEqual("<hr>");
-    expect(new Tag("img", { src: "path/to/image" }).toString()).toEqual(`<img src="path/to/image">`);
-    expect(new Tag("input", { type: "submit", value: "Save" }).toString()).toEqual(`<input type="submit" value="Save">`);
-})
 
 describe('check formFor', () => {
     test('empty form', () => {
@@ -23,24 +16,24 @@ describe('check formFor', () => {
         })).toEqual(readFixture(('formWithUrl.html')));
     })
     test('form with input as textarea', () => {
-        expect(HexletCode.formFor(template, { method: 'post' }, (f) => {
+        expect(HexletCode.formFor(template, { method: 'post' }, (f : FormBuilder) => {
             f.input('name');
             f.input('job', { as: 'textarea' });
         })).toEqual(readFixture('formWithInputAsTextarea.html'));
     })
     test('form with input class', () => {
-        expect(HexletCode.formFor(template, { method: 'post' }, (f) => {
+        expect(HexletCode.formFor(template, { method: 'post' }, (f : FormBuilder) => {
             f.input('name', { class: 'user-input'});
             f.input('job');
         })).toEqual(readFixture('formWithInputClass.html'));
     })
     test('form with textarea options', () => {
-        expect(HexletCode.formFor(template, { method: 'post' }, (f ) => {
+        expect(HexletCode.formFor(template, { method: 'post' }, (f : FormBuilder ) => {
             f.input('job', { as: 'textarea', rows: 50, cols: 50});
         })).toEqual(readFixture('formWithTextareaOptions.html'));
     })
     test('form without template field', () => {
-        expect(() => HexletCode.formFor(template, { url: '/users' }, (f) => {
+        expect(() => HexletCode.formFor(template, { url: '/users' }, (f : FormBuilder) => {
             f.input('name');
             f.input('job', { as: 'textarea' });
             f.input('age');
@@ -50,14 +43,14 @@ describe('check formFor', () => {
 
 describe('submit', () => {
     test('submit empty', () => {
-        expect(HexletCode.formFor(template, { method: 'post' }, (f) => {
+        expect(HexletCode.formFor(template, { method: 'post' }, (f : FormBuilder) => {
             f.input('name');
             f.input('job');
             f.submit();
         })).toEqual(readFixture('formSubmitEmpty.html'));
     })
     test('submit with name', () => {
-        expect(HexletCode.formFor(template, { method: 'post' }, (f) => {
+        expect(HexletCode.formFor(template, { method: 'post' }, (f : FormBuilder) => {
             f.input('name');
             f.input('job');
             f.submit('Wow');
--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from 'vitest'
 import Tag from "../src/Tag.js";
 
-test('check single tag', () => {
+test('check single tag', () : void => {
     expect(new Tag("br").toString()).toEqual("<br>");
     expect(new Tag("hr").toString()).toEqual("<hr>");
     expect(new Tag("img", { src: "path/to/image" }).toString()).toEqual(`<img src="path/to/image">`);
